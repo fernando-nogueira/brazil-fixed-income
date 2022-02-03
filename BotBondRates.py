b98773d4e7423c3e8c8a94e5bb190cdc0c6b5dcc
Luis Fernando Rabelo Nogueira <luis.nogueira@petros.com.br>
2022-02-03 19:57:12 -0300

    Código funcionando, já com as pastas conectadas entre si (puxa Anbima, entra na Base)

--- BotBondRates.py
+++ BotBondRates.py
@@ -1,76 +1,16 @@
-"""
-# Importando as bibliotecas
-import urllib.request
-from datetime import date
-import datetime as dt
-import os
-
-# Especificando o diretório que será trabalhado (pasta em que o arquivo.py está presente)
-path = os.getcwd()
-path_ima = path + '\\Dados_IMA'
-path_di = path + '\\Dados_DI'
-
-# Criamos as variáveis, que serão usadas como a checagem da criação de pastas e nomeação de arquivos
-umDia = dt.timedelta(1)
-tresDias = dt.timedelta(3)
-hoje = date.today()
-if hoje.strftime('%A') == "Monday":
-   hoje = date.today() - tresDias
-else:
-    hoje = date.today() - umDia 
-hojeData = hoje.strftime("%Y%m%d")
-mesAtual = hoje.strftime("%m")
-anoAtual = hoje.strftime("%Y")
-
-# Criando as pastas, se no caso ela não existir
-# Checa se a pasta do ano existe, se não existir, cria
-pastaAno = f'{path_ima}/{anoAtual}'
-if not os.path.exists(pastaAno):
-    os.makedirs(pastaAno)
-
-# Checa se a pasta do mês existe, se não existir, cria
-pastaMes = f'{path_ima}/{anoAtual}/{mesAtual}'
-if not os.path.exists(pastaMes):
-    os.makedirs(pastaMes)
-    
-# Baixando os dados diários do IMA
-siteAnbima = "https://www.anbima.com.br/informacoes/ima/arqs/ima_completo.xls"
-saveAnbima = f'{pastaMes}/IMA_{hojeData}.xls'
-if not os.path.exists(saveAnbima):
-    urllib.request.urlretrieve(siteAnbima, saveAnbima)
-
-################################################################################
-
-# Checa se a pasta do ano existe, se não existir, cria
-pastaAno = f'{path_di}/{anoAtual}'
-if not os.path.exists(pastaAno):
-    os.makedirs(pastaAno)
-
-# Checa se a pasta do mês existe, se não existir, cria
-pastaMes = f'{path_di}/{anoAtual}/{mesAtual}'
-if not os.path.exists(pastaMes):
-    os.makedirs(pastaMes)
-    
-# Baixando os dados diários do IMA
-siteAnbima = "https://www.anbima.com.br/informacoes/est-termo/CZ-down.asp"
-saveAnbima = f'{pastaMes}/DI_{hojeData}.xls'
-if not os.path.exists(saveAnbima):
-    urllib.request.urlretrieve(siteAnbima, saveAnbima)
-"""
-
 import urllib.request
 import datetime as dt
 import os
+import pandas as pd
 
 path = os.getcwd()
 path = str(os.getcwd()).replace("\\","/")
-path = path + '/VNA'
+path = path + '/BondRates'
 
 ytday = dt.date.today() - dt.timedelta(days=1)
 today_fmt  = ytday.strftime("%Y-%m-%d")
 year_fmt = ytday.strftime("%y")
 day_fmt = ytday.strftime("%d") 
-day_fmt
 
 def portuguese_month(today = ytday):
     dict_abv = {1: 'jan', 2: 'fev', 3:'mar', 4:'abr', 5:'mai', 6:'jun', 
@@ -80,7 +20,32 @@ def portuguese_month(today = ytday):
 
 url = f'https://www.anbima.com.br/informacoes/merc-sec/arqs/m{year_fmt}{portuguese_month()}{day_fmt}.xls'
 
-save = f'BondRatesBZ{today_fmt}.xls'
-if not os.path.exists(save):
-    urllib.request.urlretrieve(url, save)
+save = f'{path}/BondRatesMining.xls'
+urllib.request.urlretrieve(url, save)
 
+df_base = pd.read_excel(f'{path}/BondRates.xlsx')
+
+if df_base['Data'].iloc[len(df_base) - 1] == ytday.strftime("%d/%m/%Y"):
+    pass
+else: 
+    df_ltn = pd.read_excel(save, sheet_name = 'LTN', header = 3)
+    df_ltn = df_ltn[['Data de Vencimento', 'Tx. Compra', 'Tx. Venda', 'PU']].dropna()
+    df_ltn['Título'] = 'LTN'
+
+    df_lft = pd.read_excel(save, sheet_name = 'LFT', header = 3)
+    df_lft = df_lft[['Data de Vencimento', 'Tx. Compra', 'Tx. Venda', 'PU']].dropna()
+    df_lft['Título'] = 'LFT'
+
+    df_ntnb = pd.read_excel(save, sheet_name = 'NTN-B', header = 3)
+    df_ntnb = df_ntnb[['Data de Vencimento', 'Tx. Compra', 'Tx. Venda', 'PU']].dropna()
+    df_ntnb['Título'] = 'NTN-B'
+
+    df_ntnf = pd.read_excel(save, sheet_name = 'NTN-F', header = 3)
+    df_ntnf = df_ntnf[['Data de Vencimento', 'Tx. Compra', 'Tx. Venda', 'PU']].dropna()
+    df_ntnf['Título'] = 'NTN-F'
+
+    df = pd.concat([df_base, df_ltn, df_ntnf, df_lft, df_ntnb])
+    df['Data'] = ytday.strftime("%d/%m/%Y")
+    df = df.set_index('Data')
+
+    df.to_excel('BondRates/BondRates.xlsx')
